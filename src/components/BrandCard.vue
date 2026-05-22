dc92ed3fa9f8d784bfc7eeec745bcf1afda30cef
greciatello <grecia.tello1@esdmadrid.es>
2026-05-22 19:45:21 +0200

    Componente BrandCar estructura

---
+++ src/components/BrandCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <article class="brand-card">
+    <div class="card-icon">
+      <span>{{ icono }}</span>
+    </div>
+    <h3>{{ titulo }}</h3>
+    <p>{{ descripcion }}</p>
+  </article>
+</template>
+
+<script setup>
+defineProps({
+  icono:       { type: String, required: true },
+  titulo:      { type: String, required: true },
+  descripcion: { type: String, required: true },
+  enlace:      { type: String, default: '' }
+})
+</script>
